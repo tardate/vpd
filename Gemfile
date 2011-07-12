bb9f02fc0dfc9c89e531aabaad4540027acbcbc4
Paul Gallagher <gallagher.paul@gmail.com>
2011-07-12 12:58:05 +0800

    Refactor with ActiveSupport::Concern

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source "http://rubygems.org"
 
-
-#gem 'rails', '~> 3.0.7'
 gem 'activesupport', '> 3.0.7'
 gem 'activerecord', '> 3.0.7'
 gem 'pg', '~> 0.11.0'
--- lib/vpd/base.rb
+++ lib/vpd/base.rb
@@ -1,42 +1,49 @@
 module Vpd
+  extend ActiveSupport::Concern
 
-  # record the initial schema_search_path
-  def self.initial_search_path
-    @@initial_search_path ||= YAML::load(File.open('config/database.yml'))[Rails.env]["schema_search_path"]
-    @@initial_search_path ||= '"$user",public'
-  end
+  module ClassMethods
 
-  # creates +schema+ and optionally runs rails migrations to it
-  def self.create(schema,and_migrate = true)
-    conn = ActiveRecord::Base.connection
-    conn.execute("CREATE SCHEMA #{schema}") unless conn.schema_exists? schema
-    self.migrate(schema) if and_migrate
-  end
+    # Returns (and records) the initial schema_search_path.
+    def initial_search_path
+      @@initial_search_path ||= YAML::load(File.open('config/database.yml'))[Rails.env]["schema_search_path"]
+      @@initial_search_path ||= '"$user",public'
+    end
 
-  # runs rails migrations into +schema+
-  # preserves the current schema search path
-  def self.migrate(schema, to_version = nil)
-    conn = ActiveRecord::Base.connection
-    return false unless conn.schema_exists? schema
-    current_search_path = conn.schema_search_path
-    conn.schema_search_path = schema
-    ActiveRecord::Migrator.migrate('db/migrate', to_version)
-    conn.schema_search_path = current_search_path
-  end
+    # Creates +schema+ and optionally runs rails migrations to it.
+    def create(schema,and_migrate = true)
+      conn = ActiveRecord::Base.connection
+      conn.execute("CREATE SCHEMA #{schema}") unless conn.schema_exists? schema
+      self.migrate(schema) if and_migrate
+    end
 
-  # prepends +schema+ to the schema search path
-  # if verify_migration is true, will create the schema if required
-  # and ensure that it is migrated up-to-date
-  def self.activate(schema,verify_migration = false)
-    base_path = self.initial_search_path
-    self.create(schema) if verify_migration
-    conn = ActiveRecord::Base.connection
-    return false unless conn.schema_exists?(schema)
-    conn.schema_search_path = [schema, base_path].compact.join(',')
-  end
+    # Runs rails migrations into +schema+
+    # Preserves the current schema search path
+    def migrate(schema, to_version = nil)
+      conn = ActiveRecord::Base.connection
+      return false unless conn.schema_exists? schema
+      current_search_path = conn.schema_search_path
+      conn.schema_search_path = schema
+      ActiveRecord::Migrator.migrate('db/migrate', to_version)
+      conn.schema_search_path = current_search_path
+    end
 
-  def self.activate_default
-    ActiveRecord::Base.connection.schema_search_path = self.initial_search_path
+    # Activates the nominated +schema+
+    # * Prepends +schema+ to the schema search path
+    # * If +verify_migration+ is true, will create the schema if required
+    #   and ensure that it is migrated up-to-date
+    def activate(schema,verify_migration = false)
+      base_path = self.initial_search_path
+      self.create(schema) if verify_migration
+      conn = ActiveRecord::Base.connection
+      return false unless conn.schema_exists?(schema)
+      conn.schema_search_path = [schema, base_path].compact.join(',')
+    end
+
+    # Activates/returns session to default schema.
+    def activate_default
+      ActiveRecord::Base.connection.schema_search_path = self.initial_search_path
+    end
+
   end
 
 end
